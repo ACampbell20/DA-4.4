039ba402511b79a72998d7f1161f94710bfa9d83
Ashanti Campbell <acampbell20@cub.uca.edu>
2026-07-30 12:38:51 -0500

    Create Circle.h

---
+++ Circle.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Shape.h"
+#include <iostream>
+class Circle :
+    public Shape
+{
+private:
+    int radius;
+public:
+    // Constructors
+    Circle();
+    Circle(int radius);
+    Circle(Circle& otherCircle);
+    // getters and setters
+    void setRadius(int radius);
+    int getRadius();
+    double getArea();
+    // operator overloads
+    Circle& operator=(Circle& otherCircle);
+    friend std::ostream& operator<<(std::ostream& strm, Circle& circle);
+ostream& draw(ostream& strm) override;
+};
