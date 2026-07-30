b8f46d1bb0afca6b874c057cfafce1f143ff0ed7
Ashanti Campbell <acampbell20@cub.uca.edu>
2026-07-30 12:43:55 -0500

    Implement Circle class with constructors and methods

---
+++ Circle.cpp
@@ -0,0 +1,46 @@
+#include "Circle.h"
+
+// Constructors
+Circle::Circle() {
+	radius = 1;
+}
+Circle::Circle(int radius) {
+	this->radius = radius;
+}
+Circle::Circle(Circle& otherCircle) {
+	if (this != &otherCircle) {
+		this->radius = otherCircle.radius;
+	}
+}
+// getters and setters
+void Circle::setRadius(int radius) {
+	this->radius = radius;
+}
+int Circle::getRadius() {
+	return radius;
+}
+double Circle::getArea() {
+	double PI = 3.1415;
+	return PI * radius * radius;
+}
+std::ostream& Circle::draw(std::ostream& strm)
+{
+    strm << " *** " << std::endl;
+    strm << "*   *" << std::endl;
+    strm << "*   *" << std::endl;
+    strm << " *** ";
+
+    return strm;
+}
+// operator overloads
+Circle& Circle::operator=(Circle& otherCircle) {
+	if (this != &otherCircle) {
+		this->radius = otherCircle.radius;
+	}
+	return *this;
+}
+std::ostream& operator<<(std::ostream& strm, Circle& circle) {
+	strm << "A Circle with radius: " << circle.getRadius() <<
+		" and an area of " << circle.getArea();
+	return strm;
+}
