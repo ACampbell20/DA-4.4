c6f57b1982405c7a8a6e40c1601b77c9a00055c1
Ashanti Campbell <acampbell20@cub.uca.edu>
2026-07-30 12:45:30 -0500

    Implement Rectangle class with basic functionality

---
+++ Rectangle.cpp
@@ -0,0 +1,61 @@
+#include "Rectangle.h"
+#include <iostream>
+
+Rectangle::Rectangle() : Rectangle(1, 1) {}
+
+Rectangle::Rectangle(int l, int w) : length(l), width(w) {
+}
+
+Rectangle::Rectangle(Rectangle& otherRectangle) {
+	if (this != &otherRectangle) {
+		this->length = otherRectangle.length;
+		this->width = otherRectangle.width;
+	}
+}
+
+void Rectangle::setLength(int l) {
+	length = l;
+}
+
+int Rectangle::getLength() {
+	return length;
+}
+int Rectangle::getWidth() {
+	return width;
+}
+
+void Rectangle::setWidth(int w) {
+	width = w;
+}
+
+double Rectangle::getArea() {
+	return length * width;
+}
+
+Rectangle& Rectangle::operator=(Rectangle& otherRect) {
+	if (this != &otherRect) {
+		this->length = otherRect.length;
+		this->width = otherRect.width;
+	}
+	return *this;
+}
+std::ostream& operator<<(std::ostream& strm, Rectangle& rect) {
+	strm << "A Rectangle with length: " << rect.getLength() <<
+		", width: " << rect.getWidth() <<
+		" and an area of " << rect.getArea();
+	return strm;
+}
+std::ostream& Rectangle::draw(std::ostream& strm)
+{
+    for (int i = 0; i < length; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            strm << "*";
+        }
+
+        strm << std::endl;
+    }
+
+    return strm;
+}
