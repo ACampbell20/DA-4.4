8d69aa12361f7472f1890645b68c8c350c4fb6f4
Ashanti Campbell <acampbell20@cub.uca.edu>
2026-07-30 12:42:26 -0500

    Add draw method and override for Circle class

--- Circle.h
+++ Circle.h
@@ -7,15 +7,13 @@ class Circle :
 private:
     int radius;
 public:
-    // Constructors
     Circle();
     Circle(int radius);
     Circle(Circle& otherCircle);
-    // getters and setters
     void setRadius(int radius);
     int getRadius();
     double getArea();
-    // operator overloads
+    std::ostream& draw(std::ostream& strm) override;
     Circle& operator=(Circle& otherCircle);
     friend std::ostream& operator<<(std::ostream& strm, Circle& circle);
 ostream& draw(ostream& strm) override;
