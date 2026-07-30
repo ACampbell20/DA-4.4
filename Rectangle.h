890240dee0a4f7436e15a5eacf51276ddcaf9532
Ashanti Campbell <acampbell20@cub.uca.edu>
2026-07-30 12:44:48 -0500

    Add Rectangle class definition with methods

---
+++ Rectangle.h
@@ -0,0 +1,26 @@
+#ifndef RECTANGLE
+#define RECTANGLE
+#include "Shape.h"
+#include <iostream>
+
+
+class Rectangle : public Shape {
+private:
+	int length;
+	int width;
+public:
+	Rectangle();
+	Rectangle(int l, int w);
+	Rectangle(Rectangle& otherRectangle);  // *
+	int getLength();
+	int getWidth();
+	void setLength(int l);
+	void setWidth(int w);
+	double getArea();
+  std::ostream& draw(std::ostream& strm) override;
+	Rectangle& operator=(Rectangle& otherRect);
+	friend std::ostream& operator<<(std::ostream& strm, Rectangle& rect);
+};
+
+
+#endif // !RECTANGLE
