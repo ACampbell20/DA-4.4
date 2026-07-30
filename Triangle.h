822093bf1f9c964e67300108718fcebd0c47bf39
Ashanti Campbell <acampbell20@cub.uca.edu>
2026-07-30 12:46:12 -0500

    Add Triangle class definition with constructors and methods

---
+++ Triangle.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "Shape.h"
+#include <iostream>
+using namespace std;
+
+class Triangle : public Shape
+{
+private:
+	int base;
+	int height;
+public:
+	// Constructors: default, one with base and height, and copy
+	Triangle();
+	Triangle(int base, int height);
+	Triangle(Triangle& otherTriangle);
+
+	// Getters and setters for base and height and area
+	int getBase();
+	int getHeight();
+	double getArea();
+  ostream& draw(ostream& strm) override;
+	void setBase(int base);
+	void setHeight(int height);
+
+	// equals and << overload
+	Triangle& operator=(Triangle& otherTriangle);
+	friend ostream& operator<<(ostream& strm, Triangle& triangle);
+
+
+};
